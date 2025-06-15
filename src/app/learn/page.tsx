28215d9ef9b3a3950b82727eb5093d91caaa25f3
Henry Bacon <henrybacon@Henrys-MacBook-Pro.local>
2025-06-15 17:03:05 -0500

    NewPage

---
+++ src/app/learn/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: `Sooner`,
+  description: `My first clean React page`,
+};
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-4">Sooner</h1>
+      <p className="text-lg text-gray-600">{`Curious to learn more about your relationship with money? `}</p>    </main>
+  );
+}
