6b943864121bdfe4c8e62fe50b8ea13affe4e8be
Henry Bacon <henrybacon@Henrys-MacBook-Pro.local>
2025-06-15 16:55:08 -0500

    Fonts23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8">
       <h1 className="text-4xl font-bold mb-4">Sooner</h1>
-      <p className="text-lg text-gray-600">{`You're now building with Next.js + React + Tailwind!`}</p>    </main>
+      <p className="text-lg text-gray-600">{`Curious to learn more about your relationship with money? `}</p>    </main>
   );
 }
